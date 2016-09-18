d036d3262266947f66028d78d3c179559d1c89a4
bioduds <bioduds@gmail.com>
2016-09-18 08:26:57 -0300

    release update

--- install.sh
+++ install.sh
@@ -25,16 +25,16 @@ command -v perl >/dev/null 2>&1 || { echo >&2 "perl is required to install Perl6
 command -v make >/dev/null 2>&1 || { echo >&2 "make command is required to install Perl6. Please install it and try again."; exit 1; }
 test -e ~/rakudo && { echo >&2 "There is already a rakudo directory in your home directory. Please move out of the way and try again."; exit 1; }
 mkdir ~/rakudo && cd ~/rakudo
-curl -O http://rakudo.org/downloads/star/rakudo-star-2016.07.tar.gz
-tar -xzf rakudo-star-2016.07.tar.gz
-cd rakudo-star-2016.07/
+curl -O http://rakudo.org/downloads/star/rakudo-star-2016.09.tar.gz
+tar -xzf rakudo-star-2016.09.tar.gz
+cd rakudo-star-2016.09/
 perl Configure.pl --backend=moar --gen-moar
 make
 # Running the tests is optional, but advised:
 #make rakudo-test
 #make rakudo-spectest
 make install
-echo "export PATH=$HOME/rakudo/rakudo-star-2016.07/install/bin:$HOME/rakudo/rakudo-star-2016.07/install/share/perl6/site/bin:$PATH" > ~/rakudo/setpath.sh
+echo "export PATH=$HOME/rakudo/rakudo-star-2016.09/install/bin:$HOME/rakudo/rakudo-star-2016.09/install/share/perl6/site/bin:$PATH" > ~/rakudo/setpath.sh
 if [ -f ~/.bashrc ]
 then
      BASHRC=~/.bashrc
